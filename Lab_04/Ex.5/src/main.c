b32d1404c7a9ae48ec19823ce7d99a49ddcdce1f
softomi4 <72925121+softomi4@users.noreply.github.com>
2020-11-17 23:14:29 +0200

    Add files via upload

---
+++ Lab_04/Ex.5/src/main.c
@@ -0,0 +1,26 @@
+#define X 5
+
+int main()
+{
+	int y = 0;
+	
+	if(x <= -1)
+	{
+		y = -1 * x -1;
+	}
+	else if (X > -1 && X <= 0)
+	{
+		y = X + 1;
+	}
+	else if (X > 0 < 1)
+	{
+		y = -1 * X + 1;
+	}
+	else if (X > 1)
+	{
+		y = x - 1;
+	}
+	
+	
+    return 0;
+}
