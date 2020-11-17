9baf6870abc0c497ef5c8766d9185eccc89dba1d
softomi4 <72925121+softomi4@users.noreply.github.com>
2020-11-17 23:18:28 +0200

    Update main.c

--- Lab_03/Ex.5/src/main.c
+++ Lab_03/Ex.5/src/main.c
@@ -3,7 +3,7 @@ int main()
 {
 	int num1 = NUM / 100; 		//получаем первую цифру
 	int num3 = NUM % 10;	//получаем третью цифру 
-	int temp = 0;
+	float temp = 0;
 	
 	float result = num1 / num3;	// находим во сколько раз больше
 	
