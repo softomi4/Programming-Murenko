af36f44011e50f022d1b401885ec052a9715e33f
softomi4 <72925121+softomi4@users.noreply.github.com>
2020-11-25 15:49:02 +0200

    Арифметическая прогрессия

---
+++ Lab_03/Ex.6/src/main.c
@@ -0,0 +1,12 @@
+#define NUM1 50
+#define NUM2 55
+
+int main() 
+{
+	int n = NUM1 - NUM2; // n - количество суммируемых чисел.
+	int result = 0;
+
+	result = ((NUM1 + NUM2) / 2 ) * n; // формула суммы арифметической прогрессии
+
+    return 0;
+}
